acd717b486abbaceeaf25b607d8bca2f750dc14a
ajdeguzman <aljohndeguzman@gmail.com>
2015-07-13 01:38:02 +0800

    done :)

--- app/src/main/java/edu/ucuccs/ucufreshmenguide/FragmentHome.java
+++ app/src/main/java/edu/ucuccs/ucufreshmenguide/FragmentHome.java
@@ -3,165 +3,21 @@ package edu.ucuccs.ucufreshmenguide;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.pm.ActivityInfo;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-
-import com.android.volley.Cache;
-import com.android.volley.Cache.Entry;
-import com.android.volley.Request.Method;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.dexafree.materialList.cards.WelcomeCard;
-import com.dexafree.materialList.view.MaterialListView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class FragmentHome extends Fragment {
     private static final String TAG = MainActivity.class.getSimpleName();
-    private ListView listView;
-    private FeedListAdapter listAdapter;
-    private List<FeedItem> feedItems;
-    SwipeRefreshLayout mSwipeRefreshLayout;
-    private String URL_FEED = "http://ajdeguzman.x10.mx/api/feed.json";
-    WelcomeCard card;
     @SuppressLint("NewApi")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_fragment_home, container, false);
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        listView = (ListView) rootView.findViewById(R.id.list);
-        final MaterialListView mListView = (MaterialListView) rootView.findViewById(R.id.material_listview);
-        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.activity_main_swipe_refresh_layout);
-
-        feedItems = new ArrayList<FeedItem>();
-        card = new WelcomeCard(getActivity());
-        card.setTitle("Welcome to UCU!");
-        card.setSubtitle("A University where character is utmost");
-        card.setDescription("Browse the guide now");
-        card.setButtonText("OKAY");
-        card.setDismissible(true);
-        card.setBackgroundColor(Color.parseColor("#0F6C3C"));
-        card.setTitleColor(Color.parseColor("#FFFFFF"));
-        card.setSubtitleColor(Color.parseColor("#FFFFFF"));
-        card.setDescriptionColor(Color.parseColor("#FFFFFF"));
-        card.setButtonTextColor(Color.parseColor("#FFFFFF"));
-        mListView.add(card);
-
-
-        listAdapter = new FeedListAdapter(getActivity(), feedItems);
-        listView.setAdapter(listAdapter);
-        GetDataTask();
-        mSwipeRefreshLayout.setColorScheme(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                listView.invalidateViews();
-                mSwipeRefreshLayout.setRefreshing(true);
-                Log.d("Swipe", "Refreshing content");
-                GetDataTask();
-                mSwipeRefreshLayout.setRefreshing(false);
-            }
-        });
-
-
         return rootView;
-
     }
-    public void GetDataTask(){
-        // We first check for cached request
-        Cache cache = AppController.getInstance().getRequestQueue().getCache();
-        Entry entry = cache.get(URL_FEED);
 
-        if (entry != null) {
-            // fetch the data from cache
-            try {
-                String data = new String(entry.data, "UTF-8");
-                try {
-                    parseJsonFeed(new JSONObject(data));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-        } else {
-            // making fresh volley request and getting json
-            JsonObjectRequest jsonReq = new JsonObjectRequest(Method.GET,
-                    URL_FEED, null, new Response.Listener<JSONObject>() {
-
-                @Override
-                public void onResponse(JSONObject response) {
-                    VolleyLog.d(TAG, "Response: " + response.toString());
-                    if (response != null) {
-                        parseJsonFeed(response);
-                    }
-                }
-            }, new Response.ErrorListener() {
-
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    VolleyLog.d(TAG, "Error: " + error.getMessage());
-                }
-            });
-
-            // Adding request to volley request queue
-            AppController.getInstance().addToRequestQueue(jsonReq);
-        }
-    }
-    /**
-     * Parsing json reponse and passing the data to feed view list adapter
-     * */
-    private void parseJsonFeed(JSONObject response) {
-        try {
-            JSONArray feedArray = response.getJSONArray("feed");
-
-            for (int i = 0; i < feedArray.length(); i++) {
-                JSONObject feedObj = (JSONObject) feedArray.get(i);
-
-                FeedItem item = new FeedItem();
-                item.setId(feedObj.getInt("id"));
-                item.setName(feedObj.getString("name"));
-
-                // Image might be null sometimes
-                String image = feedObj.isNull("image") ? null : feedObj
-                        .getString("image");
-                item.setImge(image);
-                item.setStatus(feedObj.getString("status"));
-                item.setProfilePic(feedObj.getString("profilePic"));
-                item.setTimeStamp(feedObj.getString("timeStamp"));
-
-                // url might be null sometimes
-                String feedUrl = feedObj.isNull("url") ? null : feedObj
-                        .getString("url");
-                item.setUrl(feedUrl);
-
-                feedItems.add(item);
-            }
-            // notify data changes to list adapater
-            listAdapter.notifyDataSetChanged();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
